80cec55c277130e50d648776ba7d2070ea9b80c6
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 23:04:35 +0700

    js lab 54

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// write code here
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu",
+};
+
+// multiplyNumeric(menu, 3)
+// menu = {
+//  width: 600,
+//  height: 900,
+//  title: 'My menu'
+// };
+
+function multiplyNumeric(obj, num) {
+  for (let key in obj) {
+    if (typeof obj[key] !== "number") continue;
+    obj[key] *= num;
+  }
+}
+
+multiplyNumeric(menu, 3);
+console.log(menu);
